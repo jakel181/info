c5db6ccbf4a1db95298f7a613a7838a7036df673
jakel181 <17256688+jakel181@users.noreply.github.com>
2019-01-23 18:59:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,13 +5,6 @@ function dropit(){
   document.getElementById("dropOptions").classList.toggle("show");
 }
 
-// read hash and adjust accordingly on page load
-window.onload = function(){
- if(location.hash){
-   langSelect(location.hash.substring(1));
- }
-};
-
 // close the dropdown menu if the user clicks outside it
 window.onclick = function(e){
   if(!e.target.matches('.dropdownButton')){
@@ -25,32 +18,24 @@ window.onclick = function(e){
   }
 };
 
-// creates link objects for the dropdown
 function prep(){
   var dropdown = document.getElementById('dropOptions');
-
+  
   var option;
   for (var i in l10n){
     option = document.createElement('a');
     option.dataset.locale = i;
-    option.onclick = function(){
-        var lang = this.getAttribute('data-locale');
-        location.hash = lang;
-        langSelect(lang);
-    };
+    option.onclick = function(){languageSelect(this)};
     option.innerHTML = l10n[i].language;
     dropdown.appendChild(option);
   }
 }
-
-// switch language according to its two-letter code
-function langSelect(hash){
-  if(l10n[hash]){
-    var locale = l10n[hash];
-    document.getElementById('dropButton').innerHTML = l10n[hash].language + " ▾"; // set the button text to the selected option
-    document.getElementById('translatorCredit').innerHTML = locale.translator ? locale.translator : l10n['en'].translator;
-    document.getElementById('standardDescription').innerHTML = locale.standard ? locale.standard : l10n['en'].standard;
-    document.getElementById('uniqueDescription').innerHTML = locale.unique ? locale.unique : l10n['en'].unique;
-    document.getElementById('curatorString').innerHTML = locale.curators ? locale.curators : l10n['en'].curators;
-  }
+// when the user selects an option from the dropdown menu
+function languageSelect(l){
+  var locale = l10n[l.getAttribute('data-locale')];
+  document.getElementById('dropButton').innerHTML = l.innerHTML + " ▾"; // set the button text to the selected option
+  document.getElementById('translatorCredit').innerHTML = locale.translator ? locale.translator : l10n['en'].translator;
+  document.getElementById('standardDescription').innerHTML = locale.standard ? locale.standard : l10n['en'].standard;
+  document.getElementById('uniqueDescription').innerHTML = locale.unique ? locale.unique : l10n['en'].unique;
+  document.getElementById('curatorString').innerHTML = locale.curators ? locale.curators : l10n['en'].curators;
 }
